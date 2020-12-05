fa11241cc6075acad88004c4a6aa4738a9d4f7ab
Preston Shuffield <prshuffield@gmail.com>
2020-12-05 09:47:29 -0800

    Initial Commit

---
+++ Training 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Training 1
+//
+//  Created by Ashlan Shuffield on 12/5/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
